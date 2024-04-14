5405d582faf90333d8d288ee5d2fb31a45eb268e
gthkjs <gthkjs@gmail.com>
2024-04-14 11:31:59 +0300

    Why is the server not working? 1. I forgot the collon after localhost How do I setup my landing page? 1. set a default view engine to ejs 2. create a folder named views and add a root.ejs file 3. edit root.ejs with something 4. add the route connecting the view to the server in server.ejs file ("/)

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ import express from "express";
 
 const [server, port] = [express(), 3000];
 
+server.set("view engine", "ejs");
+
+server.get("/", (req, res) => {
+	res.render("root");
+});
+
 server.listen(port, () => {
-	console.log(`http://localhost${port}`);
+	console.log(`http://localhost:${port}`);
 });
