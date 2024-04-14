a91a0198ad96e55ec9d2ec2433b138922cb02498
gthkjs <gthkjs@gmail.com>
2024-04-14 11:44:55 +0300

    How do I add a login page? 1. select a routing method - i will use express 2. in the views folder, add login.ejs 3. in the routes folder add login.js 4. in the server folder import the login route and configure as needed

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
  */
 
 import express from "express";
+import { default as login } from "./routes/login.js";
 
 const [server, port] = [express(), 3000];
 
 server.set("view engine", "ejs");
+
+server.use("/login", login);
 
 server.get("/", (req, res) => {
 	res.render("root");
